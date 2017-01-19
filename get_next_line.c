ce09bf7782e761ecf4baed621bb7c6159560176b
Syberam <sbonnefo@student.42.fr>
2017-01-19 19:34:09 +0100

    tentative iterative

--- get_next_line.c
+++ get_next_line.c
@@ -6,45 +6,54 @@
 /*   By: sbonnefo <sbonnefo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/13 18:25:02 by sbonnefo          #+#    #+#             */
-/*   Updated: 2017/01/17 20:30:04 by sbonnefo         ###   ########.fr       */
+/*   Updated: 2017/01/19 19:33:31 by sbonnefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-static int			ft_gnl(const int fd, char **line, int nbread)
+/*
+void				ft_fill_line(char **line, char *tmp)
+{
+	if (!*line)
+		*line = ft_strsub(tmp, 0, ft_strlen(tmp)
+			- ft_strlen(ft_strchr(tmp, '\n')));
+	else
+		*line = ft_strjoin(*line,
+			ft_strsub(tmp, 0, (ft_strlen(tmp)
+				- ft_strlen(ft_strchr(tmp, '\n')))));
+	tmp = ft_strchr(tmp, '\n');
+	if (tmp)
+		tmp = tmp + 1;
+}
+*/
+int					get_next_line(const int fd, char **line)
 {
 	char			buff[BUFF_SIZE + 1];
 	static char		*tmp;
+	int				nbread;
 
-	if (!tmp)
+	*line = NULL;
+	nbread = 1;
+	if (tmp)
 	{
-		if (!(nbread = read(fd, buff, BUFF_SIZE)))
-			return (nbread);
-		buff[nbread] = 0;
-		tmp = buff;
-		return (ft_gnl(fd, &*line, nbread));
+		*line = ft_strsub(tmp, 0, ft_strlen(tmp) - ft_strlen(ft_strchr(tmp, '\n')));
 	}
 	else
 	{
-		if (!*line)
-		*line = ft_strsub(tmp, 0, ft_strlen(tmp)
-				- ft_strlen(ft_strchr(tmp, '\n')));
-		else
-		*line = ft_strjoin(*line, 
-			ft_strsub(tmp, 0, ft_strlen(tmp)
-				- ft_strlen(ft_strchr(tmp, '\n'))));
-		tmp = ft_strchr(tmp, '\n') + 1;
-		if (nbread < BUFF_SIZE && ft_strlen(tmp) == 1)
-			return (0);
-		else if (nbread == BUFF_SIZE && ft_strlen(tmp) == 1)
-			return (ft_gnl(fd, &*line, nbread));
+		while (nbread > 0)
+		{
+			if (!(nbread = read(fd, buff, BUFF_SIZE)))
+				return (1);
+			buff[BUFF_SIZE] = 0;
+			if (!tmp)
+				tmp = buff;
+			else
+				tmp = ft_strjoin(tmp, buff);
+		}
+		return (get_next_line(fd, &*line));
 	}
 	return (1);
 }
 
-int					get_next_line(const int fd, char **line)
-{
-	*line = NULL;
-	return (ft_gnl(fd, &*line, BUFF_SIZE));
-}
+tout stocker dans tmp, mais la partie envoyee dans line, decaler au second tour.
+utiliser ft_strncmp(line, tmp, ft_strlen(line));
