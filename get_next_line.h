ca4b8c91c42563441015623c44b0de16ada5a085
Syberam <sbonnefo@student.42.fr>
2017-01-21 19:26:58 +0100

    normalised

--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,17 @@
 /*   By: sbonnefo <sbonnefo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/12 12:54:53 by sbonnefo          #+#    #+#             */
-/*   Updated: 2017/01/17 20:29:37 by sbonnefo         ###   ########.fr       */
+/*   Updated: 2017/01/21 19:02:57 by sbonnefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 100
-#include <sys/types.h>
-#include <sys/uio.h>
-#include "LibFt/libft.h"
+# define BUFF_SIZE 5
+# include <sys/types.h>
+# include <sys/uio.h>
+# include "libft/libft.h"
 
 int			get_next_line(const int fd, char **line);
 
