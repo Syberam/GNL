dd73526fe46ee1db3b85c99cef348364b313c814
Syberam <sbonnefo@student.42.fr>
2017-01-24 20:37:32 +0100

    nouvelle fonction

--- get_next_line.c
+++ get_next_line.c
@@ -6,68 +6,45 @@
 /*   By: sbonnefo <sbonnefo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/13 18:25:02 by sbonnefo          #+#    #+#             */
-/*   Updated: 2017/01/21 19:55:42 by sbonnefo         ###   ########.fr       */
+/*   Updated: 2017/01/24 20:36:40 by sbonnefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-void				*ft_stock_line(char **line, char *tmp)
+/*
+int					ft_new_tmp(const int fd, char *buff, int nbread)
 {
-	int				ibn;
 
-	if (!(ft_strchr(tmp, '\n')))
-		ibn = 0;
-	else
-		ibn = ft_strlen(ft_strchr(tmp, '\n')) - 1;
-	if (*line == NULL)
-		*line = ft_strsub(tmp, 0, ft_strlen(tmp) - ibn);
-	else
-		*line = ft_strjoin(*line, ft_strsub(tmp, 0, (ft_strlen(tmp) - ibn)));
-	return (*line);
+
+	*tmp = ft_strdup(buff);
+	return (nbread);
 }
-
-static int			ft_gnl(const int fd, char **line, int nbread, char **tmp)
-{
-	char			buff[BUFF_SIZE + 1];
-
-	while ((*line == NULL || ft_strchr(*line, '\n') == NULL) && nbread > 0)
-	{
-		if (nbread < BUFF_SIZE)
-			return (1);
-		if (*tmp != NULL)
-		{
-			if (ft_strchr(*tmp, '\n') == NULL)
-			{
-				if ((nbread = read(fd, buff, BUFF_SIZE)) < 1)
-					return (nbread);
-				buff[nbread] = 0;
-				*tmp = ft_strdup(buff);
-			}
-			else
-				*tmp = ft_strchr(*tmp, '\n') + 1;
-		}
-		else
-		{
-			if ((nbread = read(fd, buff, BUFF_SIZE)) < 1)
-				return (nbread);
-			buff[nbread] = 0;
-			*tmp = ft_strdup(buff);
-		}
-		*line = ft_stock_line(&*line, *tmp);
-	}
-	if (ft_strchr(*line, '\n') != NULL)
-		*line = ft_strsub(*line, 0, (ft_strlen(*line)
-			- ft_strlen(ft_strchr(*line, '\n'))));
-	return (1);
-}
+*/
 
 int					get_next_line(const int fd, char **line)
 {
-	static char		*tmp;
+	static char		buff[BUFF_SIZE + 1];
+	size_t			mal;
+	int				nbread;
 
-	if (!line || !fd || fd < 0 || BUFF_SIZE < 1)
+	mal = 0;
+	if (!line || fd < 0 || BUFF_SIZE < 1)
 		return (-1);
 	*line = NULL;
-	return (ft_gnl(fd, &*line, BUFF_SIZE, &tmp));
+	if ((nbread = read(fd, buff, BUFF_SIZE)) < 1)
+		return (nbread);
+	buff[nbread] = 0;
+	while (ft_strchr(buff, '\n') == NULL)
+	{
+
+		if (*line != NULL)
+			mal = 1;
+		*line = ft_ext_strjoin_free(*line, buff, mal);
+		if (nbread < BUFF_SIZE)
+			return (1);
+		if ((nbread = read(fd, buff, BUFF_SIZE)) < 0)
+			return (nbread);
+		buff[nbread] = 0;
+	}
+	return (1);
 }
