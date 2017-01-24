0659deefaa5371090077641a19cc8db8a8ba89ea
Syberam <sbonnefo@student.42.fr>
2017-01-24 20:44:40 +0100

    buff size changed

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: sbonnefo <sbonnefo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/12 12:54:53 by sbonnefo          #+#    #+#             */
-/*   Updated: 2017/01/21 19:02:57 by sbonnefo         ###   ########.fr       */
+/*   Updated: 2017/01/24 19:58:00 by sbonnefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 5
+# define BUFF_SIZE 10
 # include <sys/types.h>
 # include <sys/uio.h>
 # include "libft/libft.h"
