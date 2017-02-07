044cfb050b819b6882354eee654577642ac05873
Administrateur <Administrateur@Administrateurs-MacBook-Pro.local>
2017-02-07 11:43:34 +0100

    change struct

--- get_next_line.h
+++ get_next_line.h
@@ -6,18 +6,25 @@
 /*   By: sbonnefo <sbonnefo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/12 12:54:53 by sbonnefo          #+#    #+#             */
-/*   Updated: 2017/01/24 19:58:00 by sbonnefo         ###   ########.fr       */
+/*   Updated: 2017/01/31 19:09:18 by sbonnefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 10
+# define BUFF_SIZE 5
 # include <sys/types.h>
 # include <sys/uio.h>
 # include "libft/libft.h"
 
-int			get_next_line(const int fd, char **line);
+typedef struct		s_glst
+{
+	char			*content;
+	int				fd;
+	struct s_glst	*next;
+}					t_gnl;
+
+int					get_next_line(const int fd, char **line);
 
 #endif
